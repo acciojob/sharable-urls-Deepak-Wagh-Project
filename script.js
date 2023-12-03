c4617e5920deeee95d73718ff8857aaee97152c8
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-12-03 13:33:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const name=document.getElementById("name");
 const year=document.getElementById("year");
 const url=document.getElementById("url");
 const btn=document.getElementById("button");
+const form=document.getElementById("form");
 
-btn.addEventListener("click",()=>getData(name,year));
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	getData(name,year)});
 function   getData(name,year) {
 	if(!year.value&&!name.value){
 		return;
