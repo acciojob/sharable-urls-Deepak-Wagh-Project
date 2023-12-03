a42aa8fc4403b0ef173abf5bedfe89bd7c21571a
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-12-03 12:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+const name=document.getElementById("name");
+const year=document.getElementById("year");
+const url=document.getElementById("url");
+const btn=document.getElementById("button");
+
+btn.addEventListener("click",()=>getData(name,year));
+function   getData(name,year) {
+	  text = url.innerText+"?"+name.value+year.value
+	  url.innerText=text;
+}
